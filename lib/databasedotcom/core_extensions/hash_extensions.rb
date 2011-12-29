f497df9da21bfd8ea8f28d4563c65d3a07190a78
Abhi Yerra <abhi@berkeley.edu>
2011-12-29 15:04:54 -0800

    Check to make sure that the monkey patched methods are not already defined.

--- lib/databasedotcom/core_extensions/hash_extensions.rb
+++ lib/databasedotcom/core_extensions/hash_extensions.rb
@@ -1,8 +1,10 @@
 class Hash
-  def symbolize_keys!
-    keys.each do |key|
-      self[(key.to_sym rescue key) || key] = delete(key)
+  unless respond_to?(:symbolize_keys!)
+    def symbolize_keys!
+      keys.each do |key|
+        self[(key.to_sym rescue key) || key] = delete(key)
+      end
+      self
     end
-    self
   end
 end
